22a5be2a5671d5b3c831238b79c14581f62992a4
Shubham Anand <shubham_chess@live.com>
2018-08-27 22:12:23 +0530

    add bubble sort

---
+++ class1/sorting/bubbleSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+
+    int arraySize;
+    // take array size input
+    std::cin >> arraySize;
+
+    int array[arraySize];
+
+    // take array input
+    for (int index=0; index<arraySize; index++) {
+        std::cin >> array[index];
+    }
+
+    // Printing array before sorting
+    for (int i=0; i<arraySize; i++) {
+        std::cout << array[i] << "\n";
+    }
+
+    // Proceed to sort array in ascending order
+    // TODO: Try sorting the array in descending order
+    for (int i=0; i<arraySize; i++) {
+        for (int j=i; j<arraySize; j++) {
+
+            int temp;
+            // temp is a temporary variable to store a value so as in to perform a swap.
+            if (array[i] > array[j]) {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+        }
+    }
+
+    // Printing array after sorting
+    for (int i=0; i<arraySize; i++) {
+        std::cout << array[i] << "\n";
+    }
+
+    return 0;
+}
