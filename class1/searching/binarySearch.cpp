c83e98fd8c623a7874194642222dd77d5f3edeb4
Shubham Anand <shubham_chess@live.com>
2018-08-27 22:03:40 +0530

    add binary search

---
+++ class1/searching/binarySearch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+
+    int arraySize;
+    std::cin >> arraySize;
+
+    int array[arraySize];
+
+    int elementToBeSearched;
+    std::cin >> elementToBeSearched;
+
+    // This is a flag that will keep track of whether the element we were searching was 
+    // actually present in the array or not
+    bool isElementPresent = false;
+
+    // Integers containing values of first and last index of array
+    int startIndex = 0;
+    int endIndex = arraySize-1;
+
+    while (endIndex > startIndex) {
+
+        int midIndex = (startIndex + endIndex)/2;
+
+        if (array[midIndex] == elementToBeSearched) {
+            isElementPresent = true;
+        } else if (array[midIndex] < elementToBeSearched) {
+            startIndex = midIndex + 1;
+        } else {
+            endIndex = midIndex - 1;
+        }
+
+    }
+
+    // we check to see if our flag, i.e (type: bool)isElementPresent, is true 
+    // (would only be possible if we changed it's value inside the loop that's again
+    // only possible when the search was successful) or false (initial value) using 
+    // ternary operator -> condition ? if true : if false;
+    isElementPresent ? std::cout << "Search successful\n" : std::cout << "Search unsuccessful\n";
+
+    return 0;
+}
