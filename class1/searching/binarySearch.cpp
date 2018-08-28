32bc3d3206ae8bdc625c8da79566312b1a97afa9
Shubham Anand <shubham_chess@live.com>
2018-08-28 13:16:07 +0530

    add time track to search algos

--- class1/searching/binarySearch.cpp
+++ class1/searching/binarySearch.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
+#include <chrono>
 
-int main() {
+// Donot pay any heed to this code here. Jump directly to binsearch function
+void calculateRunTime(long *array, long arraySize, long elementToBeSearched, void func(long *, long, long)) {
+    
+    typedef long long ll;
 
-    int arraySize;
-    // take array size input
-    std::cin >> arraySize;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
 
-    int array[arraySize];
+    func(array, elementToBeSearched, arraySize);
 
-    // take array input
-    for (int index=0; index<arraySize; index++) {
-        std::cin >> array[index];
-    }
+    end = std::chrono::high_resolution_clock::now();
+    ll elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    std::cout << "Elapsed Time: " << elapsed_time << "ms\n";
 
-    int elementToBeSearched;
-    std::cin >> elementToBeSearched;
+}
 
+void binSearch(long *array, long elementToBeSearched, long arraySize) {
     // This is a flag that will keep track of whether the element we were searching was 
     // actually present in the array or not
     bool isElementPresent = false;
 
     // Integers containing values of first and last index of array
-    int startIndex = 0;
-    int endIndex = arraySize-1;
+    long startIndex = 0;
+    long endIndex = arraySize-1;
 
-    while (endIndex > startIndex) {
+    while (endIndex >= startIndex) {
 
-        int midIndex = (startIndex + endIndex)/2;
+        long midIndex = (startIndex + endIndex)/2;
 
         if (array[midIndex] == elementToBeSearched) {
             isElementPresent = true;
+            break;
         } else if (array[midIndex] < elementToBeSearched) {
             startIndex = midIndex + 1;
         } else {
@@ -43,6 +46,30 @@ int main() {
     // only possible when the search was successful) or false (initial value) using 
     // ternary operator -> condition ? if true : if false;
     isElementPresent ? std::cout << "Search successful\n" : std::cout << "Search unsuccessful\n";
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+
+    long arraySize;
+    // take array size input
+    std::cin >> arraySize;
+
+    long array[arraySize];
+
+    // take array input
+    for (long index=0; index<arraySize; index++) {
+        std::cin >> array[index];
+    }
+
+    long elementToBeSearched;
+    std::cin >> elementToBeSearched;
+
+    // Just assume there is binSearch function here instead of this calculate
+    // RunTime function. This is only here to give an idea about the time
+    // complexity of binSearch algorithm
+    calculateRunTime(array, arraySize, elementToBeSearched, binSearch);
 
     return 0;
 }
--- class1/searching/linearSearch.cpp
+++ class1/searching/linearSearch.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
-int main() {
+// Donot pay any heed to this code here. Jump directly to linearSearch function
+void calculateRunTime(long *array, long arraySize, long elementToBeSearched, void func(long *, long, long)) {
+    
+    typedef long long ll;
 
-    int arraySize;
-    // take array size input
-    std::cin >> arraySize;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
 
-    int array[arraySize];
+    func(array, elementToBeSearched, arraySize);
 
-    // take array input
-    for (int index=0; index<arraySize; index++) {
-        std::cin >> array[index];
-    }
+    end = std::chrono::high_resolution_clock::now();
+    ll elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    std::cout << "Elapsed Time: " << elapsed_time << "ms\n";
 
-    int elementToBeSearched;
-    std::cin >> elementToBeSearched;
+}
 
+void linearSearch(long *array, long elementToBeSearched, long arraySize) {
     // This is a flag that will keep track of whether the element we were searching was 
     // actually present in the array or not
     bool isElementPresent = false;
 
-    for (int index=0; index<arraySize; index++) {
+    for (long index=0; index<arraySize; index++) {
 
         // Here we'll try to loop through the array comparing each of it's element with the 
         // element that needs to be searched. If we find the element, we set the value of
@@ -39,6 +40,30 @@ int main() {
     // only possible when the search was successful) or false (initial value) using 
     // ternary operator -> condition ? if true : if false;
     isElementPresent ? std::cout << "Search successful\n" : std::cout << "Search unsuccessful\n";
+}
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+
+    long arraySize;
+    // take array size input
+    std::cin >> arraySize;
+
+    long array[arraySize];
+
+    // take array input
+    for (long index=0; index<arraySize; index++) {
+        std::cin >> array[index];
+    }
+
+    long elementToBeSearched;
+    std::cin >> elementToBeSearched;
+
+    // Just assume there is binSearch function here instead of this calculate
+    // RunTime function. This is only here to give an idea about the time
+    // complexity of linearSearch algorithm
+    calculateRunTime(array, arraySize, elementToBeSearched, linearSearch);
 
     return 0;
 }
